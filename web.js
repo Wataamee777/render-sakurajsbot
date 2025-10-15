737f87a667a81daffbb794eb9e475fa86354d4b7
わたあめえ <Wataamee777@gmail.com>
2025-10-15 19:51:43 +0900

    Create web.js

---
+++ web.js
@@ -0,0 +1,7 @@
+
+const app = express();
+app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+
+const shardId = process.env.SHARD_ID || process.env.pm_id || '0'; // pm2互換対策も兼ねる
+const isMaster = shardId === '0';
