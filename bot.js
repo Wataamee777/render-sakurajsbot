7fc4b61ba6bfa65635858a9fb760fd21257f5ce7
わたあめえ <Wataamee777@gmail.com>
2025-10-15 18:59:49 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -44,7 +44,8 @@ const app = express();
 app.use(bodyParser.json());
 const PORT = process.env.PORT || 3000;
 
-const isMaster = !process.env.SHARD_ID || process.env.SHARD_ID === '0';
+const shardId = process.env.SHARD_ID || process.env.pm_id || '0'; // pm2互換対策も兼ねる
+const isMaster = shardId === '0';
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers]
