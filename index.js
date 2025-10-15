8ae652973e6b4dc14a713f00f2d0206bc7a9584b
わたあめえ <Wataamee777@gmail.com>
2025-10-15 16:39:21 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+import { ShardingManager } from 'discord.js';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const manager = new ShardingManager('./bot.js', {
+  token: process.env.DISCORD_BOT_TOKEN,
+  totalShards: 'auto', // or 数字指定
+});
+
+manager.on('shardCreate', shard => {
+  console.log(`シャード ${shard.id} が起動しました`);
+});
+
+manager.spawn();
