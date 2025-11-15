d291043505dc56018d804d7ce372140781d1c354
わたあめえ <Wataamee777@gmail.com>
2025-11-15 16:59:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+import { ShardingManager } from 'discord.js';
+import dotenv from 'dotenv';
 import './web.js';
-import './db.js';
-console.log("aaa")
+
+dotenv.config();
+
+const totalShards = 2; // ここは環境やサーバー数に応じて数値にする
+const manager = new ShardingManager('./bot.js', {
+  token: process.env.DISCORD_BOT_TOKEN,
+  totalShards: totalShards
+});
+
+manager.on('shardCreate', shard => {
+  console.log(`シャード ${shard.id} が起動しました`);
+});
+
+manager.spawn();
